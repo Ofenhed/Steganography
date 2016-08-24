ab4cf7bf4a901ed1886e148ac6a748673e29fb77
Marcus Ofenhed <marcus@conditionraise.se>
2016-08-25 00:41:53 +0200

    Improved the MAC algorigthm by calculating the hash sum on an encrypted representation of the string with a pseudo random salt to the hash algorithm, instead of just the actual string, to improve the security agagainst known plain text attacks.

--- Steganography.hs
+++ Steganography.hs
@@ -109,26 +109,34 @@ createRandomStates pixels image@(Image width height _) salt = do
 
 writeAndHash pixels image input = do
   hashPosition <- getCryptoPrimitives pixels (8 * (hashDigestSize SHA1))
-  let hash' = hashInit :: Context SHA1
+  hashSalt <- getRandomByteStringM 256
+  let hash' = hashUpdate (hashInit :: Context SHA1) $ LBS.toStrict hashSalt
       writeAndHashRecursive input' h = if LBS.length input' == 0
                                           then return $ hashFinalize h
                                           else do
                                             let byte = LBS.singleton $ LBS.head input'
                                             writeBytes pixels image byte
-                                            let newHash = hashUpdate h $ LBS.toStrict byte
+                                            macXorBytes <- getRandomByteStringM 1
+                                            let [macXorByte] = LBS.unpack macXorBytes
+                                                macByte = LBS.map (\x -> xor x macXorByte) byte
+                                                newHash = hashUpdate h $ LBS.toStrict macByte
                                             writeAndHashRecursive (LBS.tail input') newHash
   h <- writeAndHashRecursive input hash'
   writeBytes_ hashPosition image (LBS.pack $ BA.unpack h)
 
 readUntilHash pixels image = do
   hash <- readBytes pixels image (hashDigestSize SHA1)
+  hashSalt <- getRandomByteStringM 256
   let Just digest = digestFromByteString $ LBS.toStrict hash
-  let hash' = hashInit :: Context SHA1
+      hash' = hashUpdate (hashInit :: Context SHA1) $ LBS.toStrict hashSalt
       readUntilHashMatch h readData = if hashFinalize h == digest
                                          then return $ LBS.pack $ reverse readData
                                          else do
                                            b <- readBytes pixels image 1
-                                           let newHash = hashUpdate h $ LBS.toStrict b
+                                           macXorBytes <- getRandomByteStringM 1
+                                           let [macXorByte] = LBS.unpack macXorBytes
+                                               macByte = LBS.map (\x -> xor x macXorByte) b
+                                               newHash = hashUpdate h $ LBS.toStrict macByte
                                            readUntilHashMatch newHash (LBS.head b:readData)
   readUntilHashMatch hash' []
 
@@ -138,7 +146,7 @@ doEncrypt imageFile secretFile loops inputFile salt pkiFile = do
   input <- LBS.readFile inputFile
   publicKeyState <- createPublicKeyState pkiFile
   let Right (ImageRGBA8 image@(Image w h _), metadata) = decodePngWithMetadata a
-  let (newImage,_) = runST $ runRndT [(BS.bitStringLazy $ hmacSha512Pbkdf2 secret salt loops)] $ do
+      (newImage,_) = runST $ runRndT [(BS.bitStringLazy $ hmacSha512Pbkdf2 secret salt loops)] $ do
               pixels <- lift $ newRandomElementST $ getPixels (toNum w) (toNum h)
               createRandomStates pixels image salt
               mutable <- lift $ unsafeThawImage image
@@ -160,7 +168,7 @@ doDecrypt imageFile secretFile loops output salt pkiFile = do
   secret <- LBS.readFile secretFile
   privateKey <- readPrivateKey pkiFile
   let Right (ImageRGBA8 image@(Image w h _), metadata) = decodePngWithMetadata a
-  let (r,_) = runST $ runRndT [(BS.bitStringLazy $ hmacSha512Pbkdf2 secret salt loops)] $ do
+      (r,_) = runST $ runRndT [(BS.bitStringLazy $ hmacSha512Pbkdf2 secret salt loops)] $ do
               pixels <- lift $ newRandomElementST $ getPixels (toNum w) (toNum h)
               createRandomStates pixels image salt
               addAdditionalPrivateRsaState privateKey pixels image
