c4a67078a0978e778018aef9f49867607a0b344f
Marcus Ofenhed <marcus@conditionraise.se>
2016-08-29 18:56:06 +0200

    Seperated command line launcher from program logics.

---
+++ CommandLineMain.hs
@@ -0,0 +1,46 @@
+import Steganography (doEncrypt, doDecrypt)
+
+import System.Console.Command (Commands,Tree(Node),Command,command,withOption,withNonOption,io)
+import System.Console.Program (single,showUsage)
+
+import qualified Data.ByteString.Lazy.Char8 as C8
+import qualified System.Console.Argument as Argument
+
+myCommands :: Commands IO
+myCommands = Node
+  (command "steganography" "A program for hiding encrypted content in a PNG image" . io $ putStrLn "No command given, try \"steganography help\"")
+  [
+    Node encrypt [],
+    Node decrypt [],
+    Node help []
+  ]
+
+encrypt,decrypt,help :: Command IO
+encrypt = command "encrypt" "Encrypt and hide a file into a PNG file. Notice that it will overwrite the image file. SHARED-SECRET-FILE is the key. INT is the complexity of the PRNG function, higher takes longer time and is therefore more secure. INPUT-FILE is the file to be hidden in the image." $ 
+          withNonOption (namedFile "IMAGE-FILE") $ \image ->
+            withNonOption (namedFile "SHARED-SECRET-FILE") $ \secret -> 
+              withNonOption Argument.integer $ \loops ->
+                withNonOption (namedFile "INPUT-FILE") $ \file ->
+                  withOption saltOption $ \salt ->
+                    withOption pkiFileOption $ \pkiFile -> io $ doEncrypt image secret loops file (C8.pack salt) pkiFile
+
+decrypt = command "decrypt" "Get data from a PNG file. Both the SHARED-SECRET-FILE and INT has to be the same as when the file was encrypted. OUTPUT-FILE will be overwritten without warning." $
+          withNonOption (namedFile "IMAGE-FILE") $ \image ->
+            withNonOption (namedFile "SHARED-SECRET-FILE") $ \secret -> 
+              withNonOption Argument.integer $ \loops ->
+                withNonOption (namedFile "OUTPUT-FILE") $ \file ->
+                  withOption saltOption $ \salt ->
+                    withOption pkiFileOption $ \pkiFile -> io $ doDecrypt image secret loops file (C8.pack salt) pkiFile
+
+namedFile :: String -> Argument.Type FilePath
+namedFile name = Argument.Type { Argument.name = name, Argument.parser = Right, Argument.defaultValue = Nothing }
+
+pkiFileOption :: Argument.Option String
+pkiFileOption = Argument.option ['p'] ["pki"] Argument.file "" "A PEM key to use for public key cryptography. Public key for encryption and private key for decryption."
+
+saltOption :: Argument.Option String
+saltOption = Argument.option ['s'] ["salt"] Argument.string "" "A salt to be applied to the encryption."
+
+help = command "help" "Show usage info" $ io (showUsage myCommands)
+
+main = single myCommands
--- Steganography.hs
+++ Steganography.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
 
+module Steganography (doEncrypt, doDecrypt) where
+
 import BitStringToRandom (runRndT, newRandomElementST, randomElementsLength)
 import Codec.Picture.Png (decodePngWithMetadata, encodePngWithMetadata)
 import Codec.Picture.Types (dynamicMap, imageHeight, imageWidth, unsafeThawImage, unsafeFreezeImage)
@@ -13,24 +15,11 @@ import Data.Bits (shiftR, shiftL, (.|.))
 import Data.Word (Word8, Word32)
 import HashedData (readUntilHash, writeAndHash)
 import ImageFileHandler (pngDynamicMap, pngDynamicComponentCount)
-import System.Console.Command (Commands,Tree(Node),Command,command,withOption,withNonOption,io)
-import System.Console.Program (single,showUsage)
 
 import qualified Data.BitString as BS
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
-import qualified Data.ByteString.Lazy.Char8 as C8
 import qualified PixelStream
-import qualified System.Console.Argument as Argument
-
-myCommands :: Commands IO
-myCommands = Node
-  (command "steganography" "A program for hiding encrypted content in a PNG image" . io $ putStrLn "No command given, try \"steganography help\"")
-  [
-    Node encrypt [],
-    Node decrypt [],
-    Node help []
-  ]
 
 octets :: Word32 -> [Word8]
 octets w = 
@@ -88,32 +77,3 @@ doDecrypt imageFile secretFile loops output salt pkiFile = do
   case r of Nothing -> putStrLn "No hidden data found"
             Just x -> LBS.writeFile output x
 
-encrypt,decrypt,help :: Command IO
-encrypt = command "encrypt" "Encrypt and hide a file into a PNG file. Notice that it will overwrite the image file. SHARED-SECRET-FILE is the key. INT is the complexity of the PRNG function, higher takes longer time and is therefore more secure. INPUT-FILE is the file to be hidden in the image." $ 
-          withNonOption (namedFile "IMAGE-FILE") $ \image ->
-            withNonOption (namedFile "SHARED-SECRET-FILE") $ \secret -> 
-              withNonOption Argument.integer $ \loops ->
-                withNonOption (namedFile "INPUT-FILE") $ \file ->
-                  withOption saltOption $ \salt ->
-                    withOption pkiFileOption $ \pkiFile -> io $ doEncrypt image secret loops file (C8.pack salt) pkiFile
-
-decrypt = command "decrypt" "Get data from a PNG file. Both the SHARED-SECRET-FILE and INT has to be the same as when the file was encrypted. OUTPUT-FILE will be overwritten without warning." $
-          withNonOption (namedFile "IMAGE-FILE") $ \image ->
-            withNonOption (namedFile "SHARED-SECRET-FILE") $ \secret -> 
-              withNonOption Argument.integer $ \loops ->
-                withNonOption (namedFile "OUTPUT-FILE") $ \file ->
-                  withOption saltOption $ \salt ->
-                    withOption pkiFileOption $ \pkiFile -> io $ doDecrypt image secret loops file (C8.pack salt) pkiFile
-
-namedFile :: String -> Argument.Type FilePath
-namedFile name = Argument.Type { Argument.name = name, Argument.parser = Right, Argument.defaultValue = Nothing }
-
-pkiFileOption :: Argument.Option String
-pkiFileOption = Argument.option ['p'] ["pki"] Argument.file "" "A PEM key to use for public key cryptography. Public key for encryption and private key for decryption."
-
-saltOption :: Argument.Option String
-saltOption = Argument.option ['s'] ["salt"] Argument.string "" "A salt to be applied to the encryption."
-
-help = command "help" "Show usage info" $ io (showUsage myCommands)
-
-main = single myCommands
