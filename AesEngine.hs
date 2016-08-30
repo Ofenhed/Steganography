0051c0fcd945257d94f889f0a19676137cf7198e
Marcus Ofenhed <marcus@conditionraise.se>
2016-08-30 05:47:28 +0200

    Added some paranoia to the AES generation. It's not really meant to detect natural IV loops, since that will never happen, but to detect any bug that could cause an IV loop.

--- AesEngine.hs
+++ AesEngine.hs
@@ -13,9 +13,11 @@ createAes256RngState :: BySS.ByteString
                         -- ^ @aesKey@
                         -> RndState
 createAes256RngState aesKey = if (BySS.length aesKey /= 32) then error "Faulty key data"
-                                                           else  [BS.bitStringLazy $ generateStream $ initialIv]
+                                                           else  [BS.bitStringLazy $ generateStream True initialIv]
   where
   CryptoPassed cipher = cipherInit aesKey :: CryptoFailable AES256
   zeroes = BySS.pack $ replicate (blockSize cipher) 0
   initialIv = nullIV :: IV AES256
-  generateStream iv = let rand = ctrCombine cipher iv zeroes in ByS.append (ByS.fromStrict rand) (generateStream $ ivAdd iv 1)
+  generateStream firstRun iv
+   | not firstRun && (iv == initialIv) = error "AES is out of entropy."
+   | otherwise = let rand = ctrCombine cipher iv zeroes in ByS.append (ByS.fromStrict rand) (generateStream False $ ivAdd iv 1)
