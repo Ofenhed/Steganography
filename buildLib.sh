74f1b7702e808ceaca5cb775a899e7d78d9927d9
Marcus Ofenhed <marcus@conditionraise.se>
2016-11-04 07:56:16 +0100

    More refactoring

--- buildLib.sh
+++ buildLib.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 VERSION=$(ghc --version | grep -Po '\d+\.\d+\.\d+')
-cabal exec -- ghc -O2 -dynamic -shared -fPIC -o libJni.so Jni.hs module_init.c -lHSrts-ghc$VERSION
+cabal exec -- ghc -O2 -dynamic -shared -fPIC -i.:lib:src -o libJni.so lib/Jni.hs lib/module_init.c -lHSrts-ghc$VERSION
