38bf9ef257acd99c6b6ccb280866da99f1b1462a
Marcus Ofenhed <marcus@conditionraise.se>
2017-02-25 18:20:53 +0100

    Added PKI encryption to CommandLineMain, backend not yet connected.

--- src/CommandLineMain.hs
+++ src/CommandLineMain.hs
@@ -11,8 +11,8 @@ import Data.Maybe (isJust, fromJust)
 import System.Console.Command (Commands,Tree(Node),Command,command,withOption,withNonOption,io)
 import System.Console.Program (single,showUsage)
 
-import qualified Data.ByteString.Lazy.Char8 as C8
-import qualified Data.ByteString.Char8 as LC8
+import qualified Data.ByteString.Char8 as C8
+import qualified Data.ByteString.Lazy.Char8 as LC8
 import qualified System.Console.Argument as Argument
 
 myCommands :: Commands IO
@@ -33,39 +33,57 @@ readOrEmpty [] = return C8.empty
 readOrEmpty filename = C8.readFile filename
 
 doDryEncrypt' imageFile secretFile loops inputFile salt pkiFile signFile = do
-  imageData <- readOrEmpty imageFile
-  secretData <- readOrEmpty secretFile
-  inputData <- readOrEmpty inputFile
-  pkiData <- readOrEmpty pkiFile
+  imageData <- lazyReadOrEmpty imageFile
+  secretData <- lazyReadOrEmpty secretFile
+  inputData <- lazyReadOrEmpty inputFile
+  pkiData <- lazyReadOrEmpty $ pkiFile
   signData <- lazyReadOrEmpty signFile
-  let cryptoState = createDefaultCryptoState (SecretKey secretData) salt loops Nothing Nothing
+  let publicKey = if pkiData == LC8.empty
+                     then Nothing
+                     else Just pkiData
+  let privateKey = if signData == LC8.empty
+                     then Nothing
+                     else Just $ PkiSecret signData
+  let cryptoState = createDefaultCryptoState (SecretKey secretData) salt loops publicKey privateKey
   encrypted <- doEncrypt DummyContainer cryptoState DefaultHashedData imageData inputData
   case encrypted of
     Left err -> putStrLn $ "Error: " ++ err
-    Right encrypted' -> putStrLn $ "Result: " ++ (C8.unpack encrypted')
+    Right encrypted' -> putStrLn $ "Result: " ++ (LC8.unpack encrypted')
 
 doEncrypt' imageFile secretFile loops inputFile salt pkiFile signFile fastMode = do
-  imageData <- readOrEmpty imageFile
-  secretData <- readOrEmpty secretFile
-  inputData <- readOrEmpty inputFile
-  pkiData <- readOrEmpty pkiFile
+  imageData <- lazyReadOrEmpty imageFile
+  secretData <- lazyReadOrEmpty secretFile
+  inputData <- lazyReadOrEmpty inputFile
+  pkiData <- lazyReadOrEmpty pkiFile
   signData <- lazyReadOrEmpty signFile
-  let cryptoState = createDefaultCryptoState (SecretKey secretData) salt loops Nothing Nothing
+  let publicKey = if pkiData == LC8.empty
+                     then Nothing
+                     else Just pkiData
+  let privateKey = if signData == LC8.empty
+                     then Nothing
+                     else Just $ PkiSecret signData
+  let cryptoState = createDefaultCryptoState (SecretKey secretData) salt loops publicKey privateKey
   encrypted <- doEncrypt (if fastMode then PngImageSpawnerFast else PngImageSpawner) cryptoState DefaultHashedData imageData inputData
   case encrypted of
     Left err -> error err
-    Right encrypted' -> C8.writeFile imageFile encrypted'
+    Right encrypted' -> LC8.writeFile imageFile encrypted'
 
 doDecrypt' imageFile secretFile loops output salt pkiFile signFile = do
-  imageData <- readOrEmpty imageFile
-  secretData <- readOrEmpty secretFile
+  imageData <- lazyReadOrEmpty imageFile
+  secretData <- lazyReadOrEmpty secretFile
   pkiData <- lazyReadOrEmpty pkiFile
-  signData <- readOrEmpty signFile
-  let cryptoState = createDefaultCryptoState (SecretKey secretData) salt loops Nothing Nothing
+  signData <- lazyReadOrEmpty signFile
+  let publicKey = if signData == LC8.empty
+                     then Nothing
+                     else Just signData
+  let privateKey = if pkiData == LC8.empty
+                     then Nothing
+                     else Just $ PkiSecret pkiData
+  let cryptoState = createDefaultCryptoState (SecretKey secretData) salt loops publicKey privateKey
   decrypted <- doDecrypt PngImageSpawner cryptoState DefaultHashedData imageData -- pkiData signData
   case decrypted of
     Left err -> error err
-    Right decrypted' -> C8.writeFile output decrypted'
+    Right decrypted' -> LC8.writeFile output decrypted'
 
 encrypt,decrypt,help,dummyEncrypt :: Command IO
 dummyEncrypt = command "dummy" "Do a dry run encryption which outputs exactly what would have been written to the Steganography container. Options are the same as for 'encrypt', except the image file will not be modified and fast mode is no longer an option." $
@@ -75,7 +93,7 @@ dummyEncrypt = command "dummy" "Do a dry run encryption which outputs exactly wh
                 withNonOption (namedFile "INPUT-FILE") $ \file ->
                   withOption saltOption $ \salt ->
                     withOption pkiFileOption $ \pkiFile ->
-                      withOption signatureOption $ \signFile -> io $ doDryEncrypt' image secret loops file (C8.pack salt) pkiFile signFile
+                      withOption signatureOption $ \signFile -> io $ doDryEncrypt' image secret loops file (LC8.pack salt) pkiFile signFile
 
 encrypt = command "encrypt" "Encrypt and hide a file into a PNG file. Notice that it will overwrite the image file. SHARED-SECRET-FILE is the key. INT is the complexity of the PRNG function, higher takes longer time and is therefore more secure. INPUT-FILE is the file to be hidden in the image." $
           withNonOption (namedFile "IMAGE-FILE") $ \image ->
@@ -85,7 +103,7 @@ encrypt = command "encrypt" "Encrypt and hide a file into a PNG file. Notice tha
                   withOption saltOption $ \salt ->
                     withOption pkiFileOption $ \pkiFile ->
                       withOption signatureOption $ \signFile ->
-                        withOption fastMode $ \fastMode' -> io $ doEncrypt' image secret loops file (C8.pack salt) pkiFile signFile fastMode'
+                        withOption fastMode $ \fastMode' -> io $ doEncrypt' image secret loops file (LC8.pack salt) pkiFile signFile fastMode'
 
 decrypt = command "decrypt" "Get data from a PNG file. Both the SHARED-SECRET-FILE and INT has to be the same as when the file was encrypted. OUTPUT-FILE will be overwritten without warning." $
           withNonOption (namedFile "IMAGE-FILE") $ \image ->
@@ -94,7 +112,7 @@ decrypt = command "decrypt" "Get data from a PNG file. Both the SHARED-SECRET-FI
                 withNonOption (namedFile "OUTPUT-FILE") $ \file ->
                   withOption saltOption $ \salt ->
                     withOption pkiFileOption $ \pkiFile ->
-                      withOption signatureOption $ \signFile -> io $ doDecrypt' image secret loops file (C8.pack salt) pkiFile signFile
+                      withOption signatureOption $ \signFile -> io $ doDecrypt' image secret loops file (LC8.pack salt) pkiFile signFile
 
 generateKey = command "generateKey" "Generate Curve22519 keys. This will create two files, [filename].public.key and [filename].secret.key. To use these keys [filename].public.key is shared with anyone who wants to encrypt something for you. [filename].secret.key MUST NEVER BE SHARED!" $
   withNonOption (namedFile "FILENAME") $ \filename -> io $ generateKeyPair (SecretKeyPath $ filename ++ ".secret.key", PublicKeyPath $ filename ++ ".public.key")
